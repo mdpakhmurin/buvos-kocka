748923d23eae928a40cd72e4f601f641c140f069
Michael <pmdmix1@gmail.com>
2021-02-12 19:48:43 +0700

    Вращение куба

--- src/index.js
+++ src/index.js
@@ -59,56 +59,109 @@ objLoader.load(require('@static/models/cube.obj').default, ( cube ) => {
         }
     } );
     objLoader.load(require('@static/models/sticker.obj').default, ( sticker ) => {
-        sticker.traverse( function ( child ) {
-            if ( child instanceof THREE.Mesh ) {
-                child.material = new THREE.MeshPhongMaterial
-                ({
-                    color: Math.random()*0xffffff, 
-                });
-            }
-        } );
-        createBuvosKocka( buvosKocka, cube, sticker, { x: 3 , y: 3, z: 3} );
+        createBuvosKocka( buvosKocka, cube, sticker, { x: 3 , y: 3, z: 3}, [0xffb611, 'yellow', 'green', 0xff0000, 0xffffff, 0x28DAFB ] );
+        //TODO: вместо генерации материала для каждого блока генерировать материал только для каждого цвета
     });
 });
 
-function createBuvosKocka( target, cubeModel, stickerModel, size ){
+function createBuvosKocka( target, cubeModel, stickerModel, size, colors){
     function createBuvosKockaBlock(position){
         let cube = cubeModel.clone();
-        cube.position.set(position.x, position.y, position.z);
+        cube.position.set(position.x - size.x/2 + 0.5, position.y - size.y/2 + 0.5, position.z-size.z/2  + 0.5);
         target.add(cube);
 
         if ( position.x == 0){
             let sticker = stickerModel.clone();
             sticker.position.set(-0.5, 0, 0);
+
+            sticker.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    child.material = new THREE.MeshPhongMaterial
+                    ({
+                        color: colors[0]
+                    });
+                }
+            } );
+
             cube.add(sticker);
         }
         if ( position.x == size.x-1 ){
             let sticker = stickerModel.clone();
             sticker.position.set(+0.5, 0, 0);
+
+            sticker.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    child.material = new THREE.MeshPhongMaterial
+                    ({
+                        color: colors[1]
+                    });
+                }
+            } );
+
             cube.add(sticker);
         }
         if ( position.y == 0 ){
             let sticker = stickerModel.clone();
             sticker.rotation.z = Math.PI/2;
             sticker.position.set(0, -0.5, 0);
+
+            sticker.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    child.material = new THREE.MeshPhongMaterial
+                    ({
+                        color: colors[2]
+                    });
+                }
+            } );
+
             cube.add(sticker);
         }
         if ( position.y == size.y-1 ){
             let sticker = stickerModel.clone();
             sticker.rotation.z = Math.PI/2;
             sticker.position.set(0, +0.5, 0);
+
+            sticker.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    child.material = new THREE.MeshPhongMaterial
+                    ({
+                        color: colors[3]
+                    });
+                }
+            } );
+
             cube.add(sticker);
         }
         if ( position.z == 0 ){
             let sticker = stickerModel.clone();
             sticker.rotation.y = Math.PI/2;
             sticker.position.set(0, 0, -0.5);
+
+            sticker.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    child.material = new THREE.MeshPhongMaterial
+                    ({
+                        color: colors[4]
+                    });
+                }
+            } );
+
             cube.add(sticker);
         }
         if ( position.z == size.z-1 ){
             let sticker = stickerModel.clone();
             sticker.rotation.y = Math.PI/2;
             sticker.position.set(0, 0, +0.5);
+
+            sticker.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    child.material = new THREE.MeshPhongMaterial
+                    ({
+                        color: colors[5]
+                    });
+                }
+            } );
+
             cube.add(sticker);
         }
     }
@@ -135,18 +188,54 @@ function createBuvosKocka( target, cubeModel, stickerModel, size ){
     }
 }
 
-const animate = function () {
-    let time = requestAnimationFrame( animate );
+// Вращение кубика рубика
+//  Q - Влево вверх
+//  W - Вправо
+//  E - Вправо вверх
+//  С зажатым shif - в обратную сторону
+document.addEventListener('keydown', function( event ){
+    let duration = 400;
 
-    buvosKockaFly(buvosKockaFly(time));
+    let startTime = performance.now();
+    let step = 0;
 
-    renderer.render( scene, camera );
-};
+    function easeInOutCubic(x){
+        return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+    }
+    function rotateAnimation ( currentTime ){
+        let lastStep = step;
+
+        step = easeInOutCubic( ( currentTime - startTime ) / duration );
+        if (step < 1) requestAnimationFrame( rotateAnimation ); else step = 1;
+
+        switch( event.code ){
+            case 'KeyQ': 
+                buvosKocka.rotateOnWorldAxis(new THREE.Vector3( 0, 0, event.shiftKey == 1? -1: 1 ), Math.PI / 2 * (step-lastStep))
+                break;
+            case 'KeyW':
+                buvosKocka.rotateOnWorldAxis(new THREE.Vector3( 0, event.shiftKey == 1? -1: 1, 0 ), Math.PI / 2 * (step-lastStep))
+                break;
+            case 'KeyE':
+                buvosKocka.rotateOnWorldAxis(new THREE.Vector3( event.shiftKey == 1? 1: -1, 0, 0), Math.PI / 2 * (step-lastStep))
+                break;
+        }
+    }
+
+    if (["KeyQ", "KeyW", "KeyE"].indexOf(event.code) > -1)
+        requestAnimationFrame( rotateAnimation );
+});
+
 
 function buvosKockaFly( time ){
     if ( time ) {
-        buvosKocka.position.y = Math.sin( time * 0.01 ) * 0.2;
-        plane.material.opacity = 1 - ( Math.sin( time * 0.01 ) * 0.07 + 0.85);
+        buvosKocka.position.y = Math.sin( time * 0.001 ) * 0.2;
+        plane.material.opacity = 1 - ( Math.sin( time * 0.001 ) * 0.07 + 0.9);
     }
 }
+
+const animate = function () {
+    requestAnimationFrame( animate );
+    requestAnimationFrame(buvosKockaFly);
+    renderer.render( scene, camera );
+};
 animate();
