2dd8d0758661df9c77dfceb407fb6dcbae1c9c63
Michael <pmdmix1@gmail.com>
2021-02-17 21:31:14 +0700

    gihub pages

--- src/blocks/buvos-kocka/buvos-kocka__start-message/buvos-kocka__start-message.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('keydown', function( event ){
-    if (event.code == 'Space')
-        document.querySelector('.buvos-kocka__start-message').classList.add('buvos-kocka__start-message_hidden');
-})
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './style.scss';
-
-import '@blocks/buvos-kocka/buvos-kocka.js';
-import '@blocks/buvos-kocka/buvos-kocka__start-message/buvos-kocka__start-message.js'
